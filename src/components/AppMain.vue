1b9b0b3bfe3f502ed313b3d63353c1d47fbce6ec
SimoneLollini <lollinisimone@gmail.com>
2022-11-18 17:26:05 +0100

    Add scss to AppMain

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -23,26 +23,36 @@ export default {
     <main>
         <section class="contents_wrapper" v-if="store.isVisible">
             <div class="container pt-4">
-
-                <h2 class="text-white">Serie TV</h2>
-                <div class="row row-cols-4">
-                    <TvShowCard :show="show" v-for="show in store.shows" />
+                <div class="tv_show_wrapper">
+                    <h2 class="text-white">Serie TV</h2>
+                    <div class="row row-cols-4">
+                        <TvShowCard :show="show" v-for="show in store.shows" />
+                    </div>
                 </div>
                 <!-- /.tv show -->
-                <h2 class="text-white">Film</h2>
 
-                <div class="row row-cols-4">
-                    <MovieCard :movie="movie" v-for="movie in store.movies" />
+                <div class="movies_wrapper">
+                    <h2 class="text-white">Film</h2>
+                    <div class="row row-cols-4">
+                        <MovieCard :movie="movie" v-for="movie in store.movies" />
+                    </div>
                 </div>
                 <!-- /.movie -->
-
             </div>
             <!-- /.container -->
         </section>
-
     </main>
 </template>
 
 <style lang="scss" scoped>
+h2 {
+    padding-top: 1.75rem;
+    font-size: 2rem;
+    font-weight: bolder;
+    margin-bottom: -1rem;
+}
 
+.movies_wrapper {
+    padding-top: 3rem;
+}
 </style>
